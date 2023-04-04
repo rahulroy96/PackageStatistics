86d2f65d0b778cc2a2e7dfd0d1f9abd7deb2e0ad
Rahul Roy <rahulroy1996@gmail.com>
2023-04-04 03:32:10 -0400

    Refactored the contents_parser
    Moved the custom exception to within the file
    
    Parse file now returns the dictionary instead of setting the member variable

--- src/contents_parser.py
+++ src/contents_parser.py
@@ -4,22 +4,25 @@ File format can be found t https://wiki.debian.org/RepositoryFormat#A.22Contents
 import gzip
 from collections import defaultdict
 from heapq import heapify, heappop
-from exceptions import InvalidContentFileFormat
+# from exceptions import InvalidContentFileFormat
 
+
+class InvalidContentFileFormat(Exception):
+    """Raise if the format of the contents file is not correct"""
+    pass
 
 class ContentsParser:
 
     def __init__(self, filepath, table_header=True):
         self.filepath = filepath  # The path to the file that is to be parsed
-        self.files_list_per_package = defaultdict(list)
         self.table_header = table_header
 
     def parse_file(self):
         """
         Parse the contents file and create a dictionary of package -> list of files
-        :return: None
+        :return: the dictionary containing package -> list of files mappings
         """
-        self.files_list_per_package = defaultdict(list)
+        files_list_per_package = defaultdict(list)
 
         table_started = False
         with gzip.GzipFile(self.filepath) as lines:
@@ -29,8 +32,8 @@ class ContentsParser:
                 left, _, right = line.strip().rpartition(' ')
                 left = left.strip()
                 right = right.strip()
-
-                if self.table_header and not table_started:  # Loop over the lines till FILE and LOCATION is found if table_header is true
+                # Loop over the lines till FILE and LOCATION is found if table_header is true
+                if self.table_header and not table_started:
                     if left == 'FILE' and right == 'LOCATION':
                         table_started = True
                     continue
@@ -43,11 +46,13 @@ class ContentsParser:
                     # A qualified package name has the form [[$AREA/]$SECTION/]$NAME, where $AREA is the archive area,
                     # $SECTION the package section, and $NAME the name of the package.
                     _, _, package_name = qualified_package_name.rpartition('/')
-                    self.files_list_per_package[package_name].append(file)  # Append file to file list of the package
+                    files_list_per_package[package_name].append(file)  # Append file to file list of the package
 
         if self.table_header and not table_started:
             # The table header containing FILE LOCATION was not detected in the file even though table header exists
-            raise InvalidContentFileFormat("Invalid contents File! Table header - FILE LOCATION not found")
+            raise InvalidContentFileFormat("Invalid contents File format! Table header(FILE LOCATION) not found")
+
+        return files_list_per_package
 
     def top_k_packages_max_files(self, k: int) -> list:
         """
@@ -56,12 +61,12 @@ class ContentsParser:
         :return: A list (package_name, number of files) containing the k top packages
         """
         # parse the file to create the files_list_per_package dictionary
-        self.parse_file()
+        files_list_per_package = self.parse_file()
 
         # Convert the dictionary to list of tuples of the form (# of files, package name)
         # negate the number of files since we are going to use max heap
         files_per_package_count_list = [(-len(files), package_name)
-                                        for package_name, files in self.files_list_per_package.items()]
+                                        for package_name, files in files_list_per_package.items()]
 
         heapify(files_per_package_count_list)  # Heapify the list of tuples
 
