9990d4c2e1ff8f6a1e21c1aeb29d66524bca4c9b
Rahul Roy <rahulroy1996@gmail.com>
2023-04-04 03:42:46 -0400

    Implemented Package statistics cli using click library
    - Gets the architecture from command line
    
    - Downloads the file in chunk size
    
    Parses the file and prints the top 10 packages with most files

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,5 @@
+"""The constants used by package_statistics"""
+
 DOWNLOAD_FOLDER = "./downloads/"
 URL_BASE = "http://ftp.uk.debian.org/debian/dists/stable/main/"
 CHUNK_SIZE = 1024*1024
--- src/package_statistics.py
+++ src/package_statistics.py
@@ -6,24 +6,34 @@ that have the most files associated with them"""
 import click
 import os
 from utils import download_file
-from constants import DOWNLOAD_FOLDER, URL_BASE
+from constants import DOWNLOAD_FOLDER, URL_BASE, CHUNK_SIZE
+from contents_parser import ContentsParser
 
 
 @click.command()
+@click.option('--force', default=False, help='force download the content file even if it exists locally')
 @click.argument('architecture')
-def package_statistics(architecture):
-
+def package_statistics(architecture, force):
+    # Check if the download folder exists and c
     if not os.path.exists(DOWNLOAD_FOLDER):
         os.mkdir(DOWNLOAD_FOLDER)
 
-    filename = f'Contents-{architecture}.gz'
-    filepath = os.path.join(DOWNLOAD_FOLDER, filename)
-    url = f'{URL_BASE}{filename}'
+    gz_filename = f'Contents-{architecture}.gz'  # Form the filename by adding the architecture with file prefix
+    gz_filepath = os.path.join(DOWNLOAD_FOLDER, gz_filename)
+    url = f'{URL_BASE}{gz_filename}'  # Form the url by adding the filename with the base url
 
-    click.echo(f"Downloading the contents file for {architecture}.")
-    click.echo(f"Downloading {filename} using {url}")
-    download_file(url, filepath)
+    # Download the file again only is the file not present locally or if the force option is set
+    if os.path.isfile(gz_filepath) and not force:
+        click.echo(f"Using the local copy of the file")
+    else:
+        click.echo(f"Downloading the contents file for {architecture}.")
+        click.echo(f"Downloading {gz_filename} from {url}")
+        download_file(url, gz_filepath, CHUNK_SIZE)  # Download the file
 
+    content_parser = ContentsParser(gz_filepath, table_header=False)  # Initialize the ContentsParser class
+    top_10 = content_parser.top_k_packages_max_files(10)  # Get the top 10 elements
+    for i, data in enumerate(top_10):
+        click.echo(f"{i+1}. {data[0]: <35}\t{data[1]}")
 
 
 if __name__ == '__main__':
